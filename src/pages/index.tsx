e897d4ccdbf5cd9e7c713a1db904cc9853371b09
trident-kanda <tridentkandayuki@gmail.com>
2021-07-20 22:06:25 +0900

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
-import Head from "next/head";
+import { useDispatch } from "react-redux";
+import { useCounterState } from "../redux/selectors";
+import counterSlice from "../redux/slice";
 
 export default function Home() {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const state = useCounterState().counter;
+
+  const onClickIncrement = () => {
+    dispatch(counterSlice.actions.incrementCounter(1));
+  };
+
+  const onClickDecrement = () => {
+    dispatch(counterSlice.actions.decrementCounter(1));
+  };
+  return (
+    <>
+      <button onClick={onClickIncrement}>増やす</button>
+      <button onClick={onClickDecrement}>減らす</button>
+      <p>ねこが{state.count}匹いる</p>
+    </>
+  );
 }
