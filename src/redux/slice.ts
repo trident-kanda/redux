2d94f1f1f419b0515b2cdcf00ffa26fd86b132dc
trident-kanda <tridentkandayuki@gmail.com>
2021-07-22 23:47:03 +0900

    slice

--- src/redux/slice.ts
+++ src/redux/slice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { asyncIncrementCounter } from "./asyncActions";
 
 export type CounterState = {
   count: number;
@@ -26,6 +27,36 @@ const counterSlice = createSlice({
       ...state,
       count: state.count - action.payload,
     }),
+  },
+  extraReducers: (builder) => {
+    builder.addCase(asyncIncrementCounter.pending, (state) => {
+      return {
+        ...state,
+        loading: true,
+        error: false,
+        errorMessage: "",
+      };
+    });
+    builder.addCase(asyncIncrementCounter.rejected, (state, action) => {
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        errorMessage: "",
+      };
+    });
+    builder.addCase(
+      asyncIncrementCounter.fulfilled,
+      (state, action: PayloadAction<number>) => {
+        return {
+          ...state,
+          count: state.count + action.payload,
+          loading: false,
+          error: false,
+          errorMessage: "",
+        };
+      }
+    );
   },
 });
 
