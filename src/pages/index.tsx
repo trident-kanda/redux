561ae04914f482751d8395877d204c3aa334dfdc
trident-kanda <tridentkandayuki@gmail.com>
2021-07-23 20:53:41 +0900

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useDispatch } from "react-redux";
+import { asyncIncrementCounter } from "../redux/asyncActions";
 import { useCounterState } from "../redux/selectors";
 import counterSlice from "../redux/slice";
 
@@ -13,11 +14,23 @@ export default function Home() {
   const onClickDecrement = () => {
     dispatch(counterSlice.actions.decrementCounter(1));
   };
+  const onClickAsyncIncrement = async () => {
+    await dispatch(asyncIncrementCounter(10));
+  };
   return (
     <>
       <button onClick={onClickIncrement}>増やす</button>
       <button onClick={onClickDecrement}>減らす</button>
+      <button onClick={onClickAsyncIncrement} disabled={state.loading}>
+        非同期で増やす
+      </button>
       <p>ねこが{state.count}匹いる</p>
+      {state.loading ? <p>通信中</p> : ""}
+      {state.error ? (
+        <p className=" text-red-500">問題が発生。{state.errorMessage}</p>
+      ) : (
+        ""
+      )}
     </>
   );
 }
